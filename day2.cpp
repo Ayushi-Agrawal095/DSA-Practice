b56e4699e2184e23b708dccbb7e3556eb3b5198f
Ayushi Agrawal <ayushiagrawal77777@gmail.com>
2026-01-20 19:01:28 +0530

    Create day2.cpp

---
+++ day2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int n;
+     cout<<"Enter a number: "
+     cin>>n;
+     cout<<"Enter number is: "<<n<<endl;
+     return 0;
+}
+
+// take this code to print an i/p and o/p
