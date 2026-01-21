405f96bff7c95c4b20c53415a60e0231e6876634
Ayushi Agrawal <ayushiagrawal77777@gmail.com>
2026-01-21 23:22:02 +0530

    Create day3.cpp
    sum of Two num

---
+++ day3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int sumOfTwo(int a, int b){
+  int sum = a+b;
+  return sum;
+}
+int main(){
+    cout<<"Hello, World";
+    cout<<"Min of num: "<< sumOfTwo(3 , 2);
+    return 0;
+}
