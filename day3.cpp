4c15c8879492d5fc765379fcc82439ece4859876
Ayushi Agrawal <ayushiagrawal77777@gmail.com>
2026-01-21 23:32:16 +0530

    day3.a.cpp
    Added Print function to output a greeting.

--- day3.cpp
+++ day3.cpp
@@ -4,6 +4,10 @@ int sumOfTwo(int a, int b){
   int sum = a+b;
   return sum;
 }
+void Print(){
+    cout<<"Hello, Function";
+}
+
 int main(){
     cout<<"Hello, World";
     cout<<"Min of num: "<< sumOfTwo(3 , 2);
